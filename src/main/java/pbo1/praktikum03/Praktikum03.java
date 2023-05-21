6d46fa4df9c9234130302847302453caf9914e2c
iinretno <133147886+iinretno@users.noreply.github.com>
2023-05-21 18:20:51 +0800

    Membuat Atribut

--- src/main/java/pbo1/praktikum03/Praktikum03.java
+++ src/main/java/pbo1/praktikum03/Praktikum03.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package pbo1.praktikum03;
 
-/**
- *
- * @author Windows 10
- */
 public class Praktikum03 {
 
     public static void main(String[] args) {
---
+++ src/main/java/pbo1/praktikum03/Sepeda.java
@@ -0,0 +1,8 @@
+package pbo1.praktikum03;
+
+
+public class Sepeda {
+    String merk;
+    String warna;
+    Double harga;
+}
