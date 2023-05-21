fb892609b673897cb117eb0b3c7472f069d5855e
iinretno <133147886+iinretno@users.noreply.github.com>
2023-05-21 18:33:28 +0800

    Instance, Buat Atribut dan Panggil

--- src/main/java/pbo1/praktikum03/SepedaAksi.java
+++ src/main/java/pbo1/praktikum03/SepedaAksi.java
@@ -1,4 +1,11 @@
 package pbo1.praktikum03;
 public class SepedaAksi {
-    
+    public static void main(String[] args){
+        Sepeda s = new Sepeda();
+        s.merk="kupu-kupu";
+        s.warna="Merah";
+        s.harga=10000;
+        
+        s.cetakInfo();
+    }
 }
