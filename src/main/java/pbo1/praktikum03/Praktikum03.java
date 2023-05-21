8a759b24eed1ef00ebd0de976def9eb52467cd6e
iinretno <133147886+iinretno@users.noreply.github.com>
2023-05-21 18:16:40 +0800

    Initial commit

---
+++ src/main/java/pbo1/praktikum03/Praktikum03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pbo1.praktikum03;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Praktikum03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
